bb5f544205a768b56f4c67362ccafac7ae3d45d3
metrobraj <toworkbraj@gmail.com>
2026-08-20 23:22:22 +0530

    free-energy added

--- frontend.py
+++ frontend.py
@@ -1,16 +1,17 @@
 import units
 import calculations
-
+#ask user for input
 print("Need Free-energy change yeah?")
-print("well, you'll need to gimme Cu and Zn concentrations(separate by commas, in order)")
-
+print("Well, you'll need to provide Cu and Zn concentrations(separate by commas, in order)")
+#take input, split string, convert input values to float
 cuzn = input()
 cu, zn = cuzn.split(", ")
 cu = float(cu)
 zn = float(zn)
-
-print("thanks, Ecell nought value(as calculated by nernst equation) please")
+#take ecell nought value
+print("Thank you, Ecell nought value(as calculated by nernst equation) please")
 ecell0 = float(input())
-
+#gives free energy change value calculated from calculations.py
 gibb = calculations.gibbs(ecell0, zn, cu)
-print("your free energy change is", gibb)
+kgibb = gibb/1000
+print(f"Your Free-energy change is {kgibb:.3f} kJ/mol")
