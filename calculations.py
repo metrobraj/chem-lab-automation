6aacd9cc2c167db05a44622d7ce0f4ac3a331c95
metrobraj <toworkbraj@gmail.com>
2026-08-20 23:16:16 +0530

    more stuff

--- calculations.py
+++ calculations.py
@@ -1,12 +1,9 @@
 import math
 # this page holds all formulas
 
-def nernst1():
-    return emm-(0.0595/n)(math.log(gamma*c, 10))
+def nernst(ecell0, zn, cu):
+    return (ecell0 - (0.01305)*math.log(zn/cu))
 
-def nernst():
-    return ecell0 - (0.01305)*math.log(zn/cu)
-
-def gibbs():
-    return (-n)*(96500)*(nernst())
+def gibbs(ecell0, zn, cu):
+    return ((-2)*(96500)*(nernst(ecell0, zn, cu)))
 
--- frontend.py
+++ frontend.py
@@ -1,11 +1,16 @@
 import units
+import calculations
 
 print("Need Free-energy change yeah?")
 print("well, you'll need to gimme Cu and Zn concentrations(separate by commas, in order)")
 
 cuzn = input()
-cu = cuzn.split[0]
-zn=cuzn.split[1]
+cu, zn = cuzn.split(", ")
+cu = float(cu)
+zn = float(zn)
 
+print("thanks, Ecell nought value(as calculated by nernst equation) please")
+ecell0 = float(input())
 
-
+gibb = calculations.gibbs(ecell0, zn, cu)
+print("your free energy change is", gibb)
