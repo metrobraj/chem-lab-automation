c50ad785311b819edda9b442842f17fcd5f4d43a
metrobraj <toworkbraj@gmail.com>
2026-08-20 23:34:36 +0530

    free energy change for 2 temps

--- calculations.py
+++ calculations.py
@@ -1,9 +1,11 @@
 import math
 # this page holds all formulas
 
-def nernst(ecell0, zn, cu):
-    return (ecell0 - (0.01305)*math.log(zn/cu))
+def nernst(ecell0, zn, cu, T):
+    return (ecell0 - ((8.314*T)/(2*96500))*math.log(zn/cu))
 
 def gibbs(ecell0, zn, cu):
     return ((-2)*(96500)*(nernst(ecell0, zn, cu)))
 
+def enthalpy():
+    return
--- frontend.py
+++ frontend.py
@@ -1,6 +1,6 @@
-import units
 import calculations
 #ask user for input
+print("Enter [1] for free energy, [2] for average free energy at 40 degrees")
 print("Need Free-energy change yeah?")
 print("Well, you'll need to provide Cu and Zn concentrations(separate by commas, in order)")
 #take input, split string, convert input values to float
@@ -9,9 +9,14 @@ cu, zn = cuzn.split(", ")
 cu = float(cu)
 zn = float(zn)
 #take ecell nought value
-print("Thank you, Ecell nought value(as calculated by nernst equation) please")
-ecell0 = float(input())
+print("Thank you, Ecell nought values(as calculated by nernst equation) at 30 deg")
+ecell = input()
+ecell30, ecell50 = ecell.split(", ")
 #gives free energy change value calculated from calculations.py
-gibb = calculations.gibbs(ecell0, zn, cu)
-kgibb = gibb/1000
-print(f"Your Free-energy change is {kgibb:.3f} kJ/mol")
+kgibb30 = (calculations.gibbs(ecell30, zn, cu, 303))/1000
+kgibb50 = (calculations.gibbs(ecell50, zn, cu, 323))/1000
+
+print(f"Free-energy change at 30 C is {kgibb30:.3f} kJ/mol")
+print(f"Free-energy change at 50 C is {kgibb50:.3f} kJ/mol")
+
+
